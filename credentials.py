191ac9b3d1d69a1f29258a42fcfe070b9cd9e32e
Asfandyar Iqbal (Ozzy) <asfandyar424@hotmail.com>
2024-07-26 16:00:25 -0500

    Create credentials.py

---
+++ credentials.py
@@ -0,0 +1,6 @@
+TWITTER_API_KEY = 'your_api_key'
+TWITTER_API_SECRET_KEY = 'your_api_secret_key'
+TWITTER_ACCESS_TOKEN = 'your_access_token'
+TWITTER_ACCESS_TOKEN_SECRET = 'your_access_token_secret'
+EMAIL_USER = 'your_email@example.com'
+EMAIL_PASSWORD = 'your_email_password'
