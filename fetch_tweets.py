54d6bdd0862a7663bf3da69158947869bac81647
Asfandyar Iqbal (Ozzy) <asfandyar424@hotmail.com>
2024-08-25 17:21:30 -0500

    Update fetch_tweets.py

--- fetch_tweets.py
+++ fetch_tweets.py
@@ -2,17 +2,14 @@ import tweepy
 import json
 from credentials import TWITTER_API_KEY, TWITTER_API_SECRET_KEY, TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET
 
-# Twitter authentication
 auth = tweepy.OAuth1UserHandler(
     TWITTER_API_KEY, TWITTER_API_SECRET_KEY,
     TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET
 )
 api = tweepy.API(auth)
 
-# List of Twitter handles to monitor
 twitter_handles = ['stock_handle_1', 'stock_handle_2', 'stock_handle_3']
 
-# Fetch tweets and save them to a JSON file
 def fetch_tweets():
     all_tweets = []
     for handle in twitter_handles:
